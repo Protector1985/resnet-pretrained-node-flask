b5b100ba5c7cede807ec0df881248c0c6dfa692d
Protector1985 <michaelrbraun1985@gmail.com>
2024-01-07 18:18:26 -0800

    file passthrough from node to flask complete

--- src/services/PythonService.ts
+++ src/services/PythonService.ts
@@ -1,25 +1,24 @@
 require("dotenv").config();
-import axios from 'axios'
-
+import axios from "axios";
 
 class PythonService {
-
-    async sendData(data:string) {
-       try {
-        const response = await axios.post(process.env.FLASK_SERVER_URL as string, data);
-            return {
-                success: true,
-                data:response
-            }
-       } catch(err) {
-            return {
-                success: false,
-                data: err
-            }
-       }
-       
+  async sendData(data: any) {
+    try {
+      const response = await axios.post(
+        process.env.FLASK_SERVER_URL as string,
+        data,
+      );
+      return {
+        success: true,
+        data: response,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        data: err,
+      };
     }
-
+  }
 }
 
-export default PythonService
+export default PythonService;
